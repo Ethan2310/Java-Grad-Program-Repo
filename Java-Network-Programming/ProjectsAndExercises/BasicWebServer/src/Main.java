7e0ad286cd3a33dc70807a324aba7eedfcd8e20b
Ethan2310 <89082288+Ethan2310@users.noreply.github.com>
2024-03-27 17:10:20 +0200

    Add files via upload

---
+++ Java-Network-Programming/ProjectsAndExercises/BasicWebServer/src/Main.java
@@ -0,0 +1,29 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        try
+        {
+
+            ServerSocket serverSocket = new ServerSocket(80); // create a server socket object
+            boolean isStop = false;
+
+            while(!isStop) // while server is not stopped
+            {
+                Socket clientSocket = serverSocket.accept(); //accept a client
+                System.out.println("Client " + clientSocket.getInetAddress().getHostAddress() + " is connected"); // print client ip address
+                ClientThread clientThread = new ClientThread(clientSocket); // create a new thread for each client
+                clientThread.start(); //start the thread
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+
+}
